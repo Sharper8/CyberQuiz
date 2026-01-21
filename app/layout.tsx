fa7cd632249136a5c792382df99ea806d24d0471
David <david.dilipe@efrei.net>
2026-01-21 22:34:04 +0100

    remove all lovable occurences

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,8 +22,6 @@ export const metadata: Metadata = {
   },
   twitter: {
     card: "summary_large_image",
-    site: "@Lovable",
-    images: ["https://lovable.dev/opengraph-image-p98pqg.png"],
   },
 };
 
